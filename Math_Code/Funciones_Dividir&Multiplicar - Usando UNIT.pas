ada3dc235bc7ef3d9f7133cc69f96da9843d082c
AGUSTIN HERNANDEZ <AguHDz@users.noreply.github.com>
2017-07-20 23:31:50 +0200

    Update Ejemplo uso Libreria Math

--- Math_Code/Funciones_Dividir&Multiplicar - Usando UNIT.pas
+++ Math_Code/Funciones_Dividir&Multiplicar - Usando UNIT.pas
@@ -1,6 +1,6 @@
 {
 *  (C) AguHDz 18-JUL-2017
-*  Ultima Actualizacion: 18-JUL-2017
+*  Ultima Actualizacion: 20-JUL-2017
 *
 *  Compilador PicPas v.0.7.2 (https://github.com/t-edson/PicPas)
 *
@@ -25,13 +25,13 @@ begin
   resultado := DecToBCD4(resultado);
   PORTC := resultado.low;
   PORTD := resultado.high;
-  delay_ms(2000);
+  delay_ms(1000);
 end; 
 
 procedure Prueba_Tiempo_Divisiones(divisiones:word);
 begin
   while((divisiones.high OR divisiones.low) > $00) do
-    resultado := Dividir(43125,100);
+    resultado := Dividir(43,10);
     dec(divisiones);
   end;
 
@@ -51,45 +51,70 @@ begin
   PORTD:=0;
 
   
-  resultado := Dividir(215,10);         // 21   
+// OPERACION                                   RESULTADO
+//==============================               =========
+  resultado := Words_Restar(100,99);        //   1
   Print_Resultado;
-  resultado := Dividir(3223,100);       // 32
-  Print_Resultado;
-  resultado := Dividir(43125,1000);     // 43
-  Print_Resultado;
-  resultado := Dividir(9999,1);         // 9999   
-  Print_Resultado;
-
-{ 
-  PORTD := $01;
-  Prueba_Tiempo_Divisiones(word(10));
-  PORTD := $0f;
-}
-
-
-  resultado := Multiplicar(23,10) + 4;   // 234
-  Print_Resultado;
-  resultado := Multiplicar(234,10) + 5;  // 2345
-  Print_Resultado;
-  resultado := Multiplicar(345,10) + 6;  // 3456
-  Print_Resultado;
-  resultado := Multiplicar(1000,4) + Multiplicar(250,2) + Multiplicar(10,6) + 7;  // 4567
-  Print_Resultado;
-
-
-  
-  
-  resultado := Resto_Dividir(255,25);     // 5
-  Print_Resultado;
-  resultado := Resto_Dividir(60000,33);   // 6
-  Print_Resultado;
-  resultado := Resto_Dividir(10000,356);  // 32
+  resultado := Words_Restar(261,250);       //   11
+  Print_Resultado;                               
+  resultado := Words_Restar_ASM(1111,1000); //   111
+  Print_Resultado;                               
+  resultado := Words_Restar(4555,3444);     //   1111
+  Print_Resultado;                               
+                                                 
+  resultado := Words_Restar(100,98);        //   2
+  Print_Resultado;                               
+  resultado := Words_Restar_ASM(272,250);   //   22
+  Print_Resultado;                               
+  resultado := Words_Restar(1222,1000);     //   222
+  Print_Resultado;                               
+  resultado := Words_Restar_ASM(5666,3444); //   2222
+  Print_Resultado;                               
+                                                 
+  resultado := Dividir(215,10);             //   21   
+  Print_Resultado;                               
+  resultado := Dividir(3223,100);           //   32
+  Print_Resultado;                               
+  resultado := Dividir(43125,1000);         //   43
+  Print_Resultado;                               
+  resultado := Dividir(20000,2);            //   EEEE  (ERROR: excede conversion BCD)
+  Print_Resultado;                               
+  resultado := Dividir(9999,1);             //   9999
+  Print_Resultado;                               
+  resultado := Dividir(1,0);                //   EEEE  (ERROR: division por cero)
+  Print_Resultado;                               
+                                                 
+  resultado := Multiplicar(23,10) + 4;      //   234
+  Print_Resultado;                               
+  resultado := Multiplicar(234,10) + 5;     //   2345
+  Print_Resultado;                               
+  resultado := Multiplicar(345,10) + 6;     //   3456
+  Print_Resultado;                               
+                                                 
+  resultado := Multiplicar(2181,3);         //   6543
+  Print_Resultado;                               
+  resultado := Multiplicar(56,97);          //   5432
+  Print_Resultado;                               
+  resultado := Multiplicar(108,4);          //   432
+  Print_Resultado;                               
+                                                 
+  resultado := Resto_Dividir(255,25);       //   5
+  Print_Resultado;                               
+  resultado := Resto_Dividir(60000,33);     //   6
+  Print_Resultado;                               
+  resultado := Resto_Dividir(10000,356);    //   32
   Print_Resultado;
 
 //  PORTC := Compara_Numeros (2001,2000);
 
+{ 
+  PORTD := $01;
+  Prueba_Tiempo_Divisiones(word(10000));
+  PORTD := $0f;
+}
+
 {  
-  resultado := DecToBCD4(resultado);
+  resultado := DecToBCD4(8888);
   PORTC := resultado.low;
   PORTD := resultado.high;
 }
